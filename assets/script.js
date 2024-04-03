c85fb06d91790a0d8dd2d6a1012eb53438331f80
atengelsgjerd <atengelsgjerd@gmail.com>
2024-04-03 15:30:03 -0500

    added functions for dragging and dropping task cards

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+let nextId = JSON.parse(localStorage.getItem("nextId")) || 1;
+
 const taskBtn = $("#addTskBtn");
 const datePicker = $("#datePicker");
 const taskTitleInput = $("#taskTitle");
-const dueDate = $("#datePicker");
-const tasDescription = $("#taskDescription");
+const dueDateInput = $("#datePicker");
+const taskDescriptionInput = $("#taskDescription");
+const confirmTaskBtn = $("#confirmTsk");
+const toDoSection = $("#toDo");
 
 taskBtn.on("click", function(){
     $("#formModal").modal("show");
@@ -15,8 +18,58 @@ taskBtn.on("click", function(){
         $("#datePicker").datepicker();
     });
 
-    
+    confirmTaskBtn.on("click", function(){
+        
+        const modalObject = {
+            id: nextId,
+            taskTitle: taskTitleInput.val(),
+            taskDueDate: dueDateInput.val(),
+            taskDescription: taskDescriptionInput.val(),
+
+        };
+
+        taskList.push(modalObject);
+        localStorage.setItem("tasks", JSON.stringify(taskList));
+        localStorage.setItem("nextId", nextId + 1);
+
+        console.log(JSON.parse(localStorage.getItem("tasks")));
+    });
 });
+
+taskList.forEach((task) => {
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("card", "draggable", "text-center", "mb-3");
+    taskElement.style.width = "18rem";
+    taskElement.innerHTML = `
+    <h5 class="card-title">${task.taskTitle}</h5>
+    <h6>${task.taskDueDate}</h6>
+    <p class="card-text">${task.taskDescription}</p>
+    `;
+    taskElement.style.position = "absolute";
+    taskElement.style.zIndex = 1;
+    toDoSection.append(taskElement);
+
+    $(taskElement).draggable({
+        start: function(event, ui) {
+            $(this).css("z-index", 1000);
+        },
+        stop: function(event,ui) {
+            $(this).css("z-index", 1);
+        }
+    });
+});
+
+    $(document).ready(function(){
+        $(".droppable").droppable({
+            drop: function(event,ui) {
+                $(this)
+                ui.draggable.css("z-index", 1000);
+            }
+        });
+    });
+    
+    
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
