8dd9fc933299ea7f2eb8c1ac3d6f7b69bd952714
atengelsgjerd <atengelsgjerd@gmail.com>
2024-07-10 16:01:38 -0500

    cards now appear in center of progress columns

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,16 @@ const dueDateInput = $("#datePicker");
 const taskDescriptionInput = $("#taskDescription");
 const confirmTaskBtn = $("#confirmTsk");
 const toDoSection = $("#toDo");
+const inProgress = $("#in-progress");
+const completed = $("#completed");
+
+function getDaysDifference(dueDate) {
+    const oneDay = 24 * 60 * 60 * 1000;
+    const currentDate = new Date();
+    const dueDateObj = new Date(dueDate);
+    const diffDays = Math.round(Math.abs((currentDate - dueDateObj) / oneDay));
+    return diffDays;
+}
 
 //When the button with the id #addTskBtn is clicked, a modal form will show
 taskBtn.on("click", function(){
@@ -27,49 +37,16 @@ taskBtn.on("click", function(){
             taskTitle: taskTitleInput.val(),
             taskDueDate: dueDateInput.val(),
             taskDescription: taskDescriptionInput.val(),
-
+            status: "toDoo"
         };
-
+        nextId++;
         taskList.push(modalObject);
         localStorage.setItem("tasks", JSON.stringify(taskList));
-        localStorage.setItem("nextId", nextId + 1);
+        localStorage.setItem("nextId", nextId);
 
-        taskList.forEach((task) => {
-
-        const taskElement = document.createElement("div");
-        taskElement.dataset.taskId = task.id;
-        taskElement.classList.add("card", "draggable", "text-center", "mb-3");
-        taskElement.style.width = "18rem";
-        taskElement.innerHTML = `
-        <h5 class="card-title">${task.taskTitle}</h5>
-        <h6>${task.taskDueDate}</h6>
-        <p class="card-text">${task.taskDescription}</p>
-        <button class="btn btn-primary" id="deleteBtn">Delete</button>
-        `;
-
-        toDoSection.append(taskElement);
-
-        console.log(JSON.parse(localStorage.getItem("tasks")));
-
-        $(taskElement).draggable({
-            start: function(event, ui) {
-                $(this).css("z-index", 1000);
-            },
-            stop: function(event,ui) {
-                $(this).css("z-index", 1);
-            }
-        });
-        toDoSection.on("click", ".card #deleteBtn", function(){
-            const taskId = $(this).closest(".card").data("task-id");
-            taskList = taskList.filter(task => task.id !== taskId);
-            localStorage.setItem("tasks", JSON.stringify(taskList));
-            $(this).parent().remove();
-        });
-    });
+        renderTaskList();
 });
    
-    //taskElement.style.position = "absolute";
-    //taskElement.style.zIndex = 1;
     
 
    
@@ -80,14 +57,27 @@ taskBtn.on("click", function(){
             drop: function(event,ui) {
                 $(this)
                 ui.draggable.css("z-index", 1000);
+                console.log(ui.draggable.data("task-id"));
+                console.log(event.target.id);
+                taskList.forEach((task) => {
+                    if (task.id === ui.draggable.data("task-id")) {
+                        task.status = event.target.id;
+                    }
+                    
+                });
+                localStorage.setItem("tasks", JSON.stringify(taskList));
+                renderTaskList();
             }
         });
+        renderTaskList();
     });
+
     
     
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
+    
 
 }
 
@@ -99,6 +89,65 @@ function createTaskCard(task) {
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
 
+
+    $('#todo-cards').empty();
+    $('#in-progress-cards').empty();
+    $('#done-cards').empty();
+    taskList.forEach((task) => {
+
+        const taskElement = document.createElement("div");
+        taskElement.dataset.taskId = task.id;
+        taskElement.classList.add("card", "draggable", "text-center", "mb-3");
+
+        const daysDifference = getDaysDifference(task.taskDueDate);
+
+        if (daysDifference <= 0) {
+            taskElement.classList.add("overdue");
+        } else if (daysDifference <= 2) {
+            taskElement.classList.add("near-due");
+        }
+        taskElement.style.width = "18rem";
+        taskElement.innerHTML = `
+        <h5 class="card-title">${task.taskTitle}</h5>
+        <span>${task.id}</span>
+        <h6>${task.taskDueDate}</h6>
+        <p class="card-text">${task.taskDescription}</p>
+        <button class="btn btn-primary" id="deleteBtn">Delete</button>
+        `;
+
+        if (task.status === "toDoo") {
+            $('#todo-cards').append(taskElement);
+        } else if (task.status === "inProgress") {
+            $('#in-progress-cards').append(taskElement);
+        } else {
+            $('#done-cards').append(taskElement);
+        }
+        
+
+    console.log(JSON.parse(localStorage.getItem("tasks")));
+
+    // taskElement.forEach((task) => {
+    //     task.addEventListener("dragstart", () => {
+    //         task.classList.add("is-dragging");
+    //     });
+    // });
+
+    $(taskElement).draggable({
+        start: function(event, ui) {
+            $(this).css("z-index", 1000);
+        },
+        stop: function(event,ui) {
+            $(this).css("z-index", 1);
+        }
+        
+    });
+    toDoSection.on("click", ".card #deleteBtn", function(){
+        const taskId = $(this).closest(".card").data("task-id");
+        taskList = taskList.filter(task => task.id !== taskId);
+        localStorage.setItem("tasks", JSON.stringify(taskList));
+        $(this).parent().remove();
+    });
+});
 }
 
 // Todo: create a function to handle adding a new task
